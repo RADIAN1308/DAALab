3ea4c1fbb49256e31aeec6a772d3a430bd014261
Adrian Brine <adrian.brine135@gmail.com>
2025-11-02 21:20:47 +0530

    comments added to merge sort

--- src/Merger.java
+++ src/Merger.java
@@ -1,25 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 public class Merger {
     public static void mergeSort(int [] arr,int l, int h){
        if(l<h){
             int m= (l+h)/2;
+            //left half
             mergeSort(arr,l,m);
+            //right half
             mergeSort(arr,m+1,h);
+            //merge after reaching end elements
             merge(arr,l,m,h);
         }
     }
     public static void merge(int [] arr,int l,int m, int h){
+        //
         int n1= m+1-l;
         int n2 = h-m;
         int []L = new int[n1];
         int []R = new int[n2];
 
+        //copy left and right half of the section being sorted.
         System.arraycopy(arr, l, L, 0, n1);
-
         System.arraycopy(arr,m+1,R,0,n2);
 
+        //compare and insert the elements.
         int i = 0 ,j = 0,k=l;
         while(i<n1&&j<n2){
             if(L[i]<=R[j]){
@@ -29,6 +32,7 @@ public class Merger {
                 arr[k++]=R[j++];
         }
 
+        //extra elements to be inserted.
         while(i<n1)
             arr[k++] = L[i++];
         while(j<n2)
@@ -36,14 +40,17 @@ public class Merger {
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        //accept array size and array elements
         int n = sc.nextInt();
         int []arr = new int[n];
         for(int i = 0 ; i < n;i++){
             arr[i] =sc.nextInt();
         }
+        //begin mergesort
         mergeSort(arr,0,n-1);
-        for (int x:arr){
 
+        //output
+        for (int x:arr){
             System.out.print(x+" " );
         }
     }
