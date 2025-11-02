e90195358f1d3362c9e4f11faab199322bca2e0b
Adrian Brine <adrian.brine135@gmail.com>
2025-11-02 22:38:30 +0530

    comments added to quick sort

--- src/Quick.java
+++ src/Quick.java
@@ -4,13 +4,15 @@ public class Quick {
         int pivot = arr[l];
         int i =l,j = h;
         while(i<j){
-            while(i<h&&arr[i]<=pivot)i++;
-            while(j>l&&arr[j]>pivot)j--;
+            //pivot is greater than the left half of the array and lesser than right half.
+            while(i<h&&arr[i]<=pivot)i++;//left half lesser
+            while(j>l&&arr[j]>pivot)j--;// right half greater
             if(i<j){
+                //swaps out terms to positions agreeing to fit the pivot element.
                 swap(arr, i, j);
             }
         }
-        swap(arr,l,j);
+        swap(arr,l,j);//if all elements are in the right positions j will be lesser than i.
         return j;
     }
     public static void swap(int[]arr,int i , int j ){
@@ -21,6 +23,7 @@ public class Quick {
     public static void quickSort(int[]arr,int l,int h){
         if(l<h){
             int pivot=partition(arr,l,h);
+            //pivot is not inclusive since it is in a fixed position.
             quickSort(arr,l,pivot-1);
             quickSort(arr,pivot+1,h);
         }
